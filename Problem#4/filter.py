c4e9fa1643c3b57a1257381ed3b280f2cd54df2d
Jacob Oliver <bjo8656@utulsa.edu>
2024-02-28 14:42:06 -0600

    sd

--- Problem#4/filter.py
+++ Problem#4/filter.py
@@ -17,7 +17,7 @@ def process_and_print(input_string):
   print(sort_list)
   """"
   sort_list = sort_list.sort()
-  sort_list = sort_list [::-1]
+  sort_list = sort_list
 """
     # Print sorted integers
   #print(sort_list)
