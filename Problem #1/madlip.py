bd78e13af5cb39988de66e2fa455930c13cbeac8
Jacob Oliver <bjo8656@utulsa.edu>
2024-02-28 12:44:59 -0600

    2

--- Problem #1/madlip.py
+++ Problem #1/madlip.py
@@ -2,6 +2,8 @@ def food_input():
     user_input = input()
     tokens = user_input.split()
     #type you while  loop here
+    while (user_input != quit):
+        print()
 
     
 
--- Problem#2/Palindrome.py
+++ Problem#2/Palindrome.py
@@ -1,5 +1,11 @@
 def check_palindrome(user_input):
  #type your code here
+    input_user = user_input [::-1]
+    if (input_user == user_input):
+        print("palindrome: "+user_input)
+    elif (input_user != user_input):
+        print("not a palindrome: "+user_input)
+
 if __name__ == "__main__":
     user_input = input()
     check_palindrome(user_input)
