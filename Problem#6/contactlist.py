e2844a3ce1f2e93c3678f574f88ecb05aa86b745
Jacob Oliver <bjo8656@utulsa.edu>
2024-02-29 10:47:37 -0600

    sad

--- Problem#6/contactlist.py
+++ Problem#6/contactlist.py
@@ -1,5 +1,5 @@
 def process_user_contacts(user_input):
-    user_contacts = dict(subString.split(",") for subString in str.split(" "))
+    user_contacts = dict(user_input.split(",") for subString in user_input.split(" "))
     print(user_contacts)
 
   #  user_input = 
