e4e9b69d40b84fd1b57ee69deff342e0b289321a
Jacob Oliver <bjo8656@utulsa.edu>
2024-02-29 13:01:08 -0600

    XXZ

--- Problem#8/check.py
+++ Problem#8/check.py
@@ -11,15 +11,15 @@ def in_order(nums):
 
 if __name__ == '__main__':
     # Test out-of-order example
-    nums1 = [5, 6, 7, 8, 3]
-if in_order(nums1):
+    nums = [5, 6, 7, 8, 3]
+if in_order(nums):
     print('In order')
 else:
     print('Not in order')
         
     # Test in-order example
-    nums2 = [5, 6, 7, 8, 10]
-if in_order(nums2):
+    nums = [5, 6, 7, 8, 10]
+if in_order(nums):
     print('In order')
 else:
     print('Not in order')
