839e09e51000642cf332e2be062f4eaa320b5d2a
Jacob Oliver <bjo8656@utulsa.edu>
2024-03-01 15:55:35 -0600

    aasdasd

--- Problem #1/madlip.py
+++ Problem #1/madlip.py
@@ -5,9 +5,20 @@ Lab time: 2/28.2024 12:30
 def food_input():
     user_input = input()
     tokens = user_input.split()
+    storage = []
+
     #type you while  loop here
     while (user_input != quit):
-        print()
+        if (user_input != quit):
+            storage.append("Eating "+ str(tokens[1]) + " " +str(tokens[0]) + " a day keeps you happy and healthy.")
+        user_input = input()
+        tokens = user_input.split()
+        user_input= tokens[0]
+    sep = "\n"
+    storage = sep.join(storage)
+    print(storage)
+
+
 
     
 
