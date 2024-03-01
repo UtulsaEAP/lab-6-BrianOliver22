858941a4f7fd43ad3127441eb9b25acf354c717a
Jacob Oliver <bjo8656@utulsa.edu>
2024-03-01 17:38:45 -0600

    CcXCZ

--- Problem#5/elementsrange.py
+++ Problem#5/elementsrange.py
@@ -6,7 +6,7 @@ def filter_and_print_range(input_list, min_val, max_val):
     #write your code here
     my_list = []
     for i in range(len(input_list)):
-        input_list[i] = eval(input_list[i])
+        #input_list[i] = eval(input_list[i])
         if (input_list[i] <= max_val) and (input_list[i] >= min_val):
             my_list.append(str(input_list[i]))
    # print(my_list)
