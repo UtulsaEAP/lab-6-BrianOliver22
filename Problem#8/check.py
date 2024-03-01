fd438026b0c12bfdbc895a81879db68a0e54f4b1
Jacob Oliver <bjo8656@utulsa.edu>
2024-03-01 15:18:01 -0600

    asddas

--- Problem#8/check.py
+++ Problem#8/check.py
@@ -1,29 +1,49 @@
 """"
 Name: Brian Oliver
-Lab time: 2/29/2024 12:40
+Lab time: 3/1/2024 3:20
 """
 def in_order(nums):
+
     # Type your code here.
-    check = True
-    order = 0
-    for i in range(len(nums)):
-        if (order < nums[i]):
-            order = nums[i]
-        elif (order > nums[i]):
-            check = False
-    return check
+
+    #check = True
+
+    #order = 0
+
+    for i in range(1, len(nums)):
+
+        if nums[i - 1] > nums[i]:
+
+            return False
+
+    return True
+
+ 
 
 if __name__ == '__main__':
+
     # Test out-of-order example
+
     nums1 = [5, 6, 7, 8, 3]
+
 if in_order(nums1):
+
     print('In order')
+
 else:
+
     print('Not in order')
-        
+
+       
+
     # Test in-order example
+
     nums2 = [5, 6, 7, 8, 10]
+
 if in_order(nums2):
+
     print('In order')
+
 else:
+
     print('Not in order')
