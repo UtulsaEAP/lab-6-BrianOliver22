9af4524bd87de67ac1b13a478062d32459f9f73a
Jacob Oliver <bjo8656@utulsa.edu>
2024-03-01 16:54:24 -0600

    asasd

--- Problem#4/filter.py
+++ Problem#4/filter.py
@@ -14,13 +14,18 @@ def process_and_print(input_string):
   for i in range(len(input_data)):
     if (input_data[i]<0):
       sort_list.append(input_data[i])
- # print(sort_list)
-  sort_list = sort_list.sort()
-  print(sort_list)
-  sort_list = sort_list
-  print(sort_list)
+  # print(sort_list)
+  """"
+  sorted_list = sort_list
+  print(sorted_list)
+  """
+  sorted_list = sorted(sort_list)
+  #print(sorted_list)
+  sep = " "
+  sorted_list = sep.join(sorted_list)
+  print(sorted_list)
     # Print sorted integers
-  #print(sort_list)
+  #print(sort_list)                10 -7 4 -39 -6 12 -2
     # Sort integers in reverse order
   lowest_number = 0
   """"
@@ -33,7 +38,7 @@ def process_and_print(input_string):
 
 if __name__ == "__main__":
     # User inputs string w/ numbers
-    user_input = input("Enter a space-separated string of numbers: ")
+    user_input = str(input("Enter a space-separated string of numbers: "))
 
     # Call the function to process and print the result
     process_and_print(user_input)
